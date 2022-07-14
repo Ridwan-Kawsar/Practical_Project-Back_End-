5750b042a9409b6fa15aeab39c6d2075626ebeec
Ridwan-Kawsar <ridwan_kawsar@hotmail.co.uk>
2022-07-14 12:33:36 +0100

    Created a new Spring Starter Project - PracticalProjectBackEnd

---
+++ PracticalProjectBackEnd/src/main/java/com/qa/practicalprojectbackend/PracticalProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.qa.practicalprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ PracticalProjectBackEnd/src/test/java/com/qa/practicalprojectbackend/PracticalProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.practicalprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
