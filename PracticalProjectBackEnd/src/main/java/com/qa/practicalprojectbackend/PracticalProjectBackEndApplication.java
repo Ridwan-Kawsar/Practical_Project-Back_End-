1d3e77965568a9f5baea57e8f025cc975043023f
Ridwan-Kawsar <ridwan_kawsar@hotmail.co.uk>
2022-07-16 13:18:10 +0100

    added logic

--- PracticalProjectBackEnd/src/main/java/com/qa/practicalprojectbackend/PracticalProjectBackEndApplication.java
+++ PracticalProjectBackEnd/src/main/java/com/qa/practicalprojectbackend/PracticalProjectBackEndApplication.java
@@ -2,12 +2,16 @@ package com.qa.practicalprojectbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.qa.rest.FantasyF1Controller;
 
 @SpringBootApplication
 public class PracticalProjectBackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PracticalProjectBackEndApplication.class, args);
+		ApplicationContext context = SpringApplication.run(PracticalProjectBackEndApplication.class, args);
+		System.out.println("Found the bean: " + context.getBean(FantasyF1Controller.class));
 	}
 
 }
--- PracticalProjectBackEnd/src/main/java/com/qa/rest/FantasyF1Controller.java
+++ PracticalProjectBackEnd/src/main/java/com/qa/rest/FantasyF1Controller.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,7 +21,7 @@ import com.qa.entity.FantasyF1;
 public class FantasyF1Controller {
 
 	private List<FantasyF1> fantasyf1 = new ArrayList<>();
-	
+
 	@GetMapping("/hello")
 	public String helloWorld() {
 		String a = "Hello, ";
@@ -32,27 +34,56 @@ public class FantasyF1Controller {
 		return new FantasyF1("Ridwan Kawsar", "Mercedes", "McLaren", "Charles Leclerc", "Lewis Hamilton");
 	}
 
+	@GetMapping("/getFantasyF1/{id}")
+	public FantasyF1 getById(@PathVariable int id) {
+		return this.fantasyf1.get(id);
+	}
+
+	@GetMapping("/getFantasyF1/{id}")
+	public List<FantasyF1> getAll() {
+		return this.fantasyf1;
+	}
+
 	@PostMapping("/createFantasyF1")
-	public FantasyF1 create(@RequestBody FantasyF1 f1) {
+	public ResponseEntity<FantasyF1> create(@RequestBody FantasyF1 f1) {
 		System.out.println("Created: " + f1);
 		this.fantasyf1.add(f1);
-		return this.fantasyf1.get(this.fantasyf1.size() -1);
+		FantasyF1 created = this.fantasyf1.get(this.fantasyf1.size() - 1);
+
+		return new ResponseEntity<FantasyF1>(created, HttpStatus.CREATED); // Setting the Status Code 201; Created
 	}
-	
+
 	@PatchMapping("/updateFantasyF1{id}")
-	public void update(@PathVariable("id") int id, @PathParam("teamPrinciple") String teamPrinciple, @PathParam("engineProvider") String engineProvider, 
-			@PathParam("teamName") String teamName, @PathParam("driverOne") String driverOne, @PathParam("driverTwo") String driverTwo){
+	public FantasyF1 update(@PathVariable("id") int id, @PathParam("teamPrinciple") String teamPrinciple,
+			@PathParam("engineProvider") String engineProvider, @PathParam("teamName") String teamName,
+			@PathParam("driverOne") String driverOne, @PathParam("driverTwo") String driverTwo) {
 		System.out.println("ID: " + id);
 		System.out.println("Team Principle: " + teamPrinciple);
 		System.out.println("Engine Provider: " + engineProvider);
 		System.out.println("Team Name: " + teamName);
 		System.out.println("Driver 1: " + driverOne);
 		System.out.println("Driver 2: " + driverTwo);
+
+		FantasyF1 toUpdate = this.fantasyf1.get(id);
+		if (teamPrinciple != null) // if statements used, when updating and some field remains the same, user
+									// doesn't have to enter a value in again as the default value will be entered.
+			toUpdate.setTeamPrinciple(teamPrinciple);
+		if (engineProvider != null)
+			toUpdate.setEngineProvider(engineProvider);
+		if (teamName != null)
+			toUpdate.setTeamName(teamName);
+		if (driverOne != null)
+			toUpdate.setDriverOne(driverOne);
+		if (driverTwo != null)
+			toUpdate.setDriverTwo(driverTwo);
+		return toUpdate;
 	}
-	
+
 	// id = 4994
 	@DeleteMapping("/removeFantasyF1{id}")
-	public void delete(@PathVariable int id) {
+	public ResponseEntity<?> delete(@PathVariable int id) {
 		System.out.println("ID: " + id);
+		this.fantasyf1.remove(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
